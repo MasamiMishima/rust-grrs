14d74191bf3fbc67778ac71792c631e4db38ffcb
MasamiMishima <mishima.masami@willgate.co.jp>
2020-02-08 19:07:20 +0900

    refactor:

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,16 @@ fn main() -> Result<(), ExitFailure>  {
     let content = std::fs::read_to_string(&args.path)
             .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
 
-    Ok(for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    })
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
 }
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
